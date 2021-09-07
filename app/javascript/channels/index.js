0dfe8aac429775a412facdc592ce73b527f42479
unknown <samuelwinbiz@gmail.com>
2021-09-07 16:46:12 -0500

    new update

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,13 +1,5 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const channels = require.context('.', true, /_channel\.js$/)
+channels.keys().forEach(channels)
